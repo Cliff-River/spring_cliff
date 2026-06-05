72a04604cbdb498114ef5fc9e87555b0e4455450
Cliff Huang <hzf510920@163.com>
2026-06-05 19:04:34 +0800

    Initial commit.

---
+++ src/main/java/com/example/spring_cliff/SpringCliffApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cliff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCliffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCliffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_cliff/SpringCliffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_cliff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCliffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
