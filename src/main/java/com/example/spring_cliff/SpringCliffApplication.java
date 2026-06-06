35778f7cb0e391d6127067c3f065680624a3539d
Cliff Huang <hzf510920@163.com>
2026-06-06 14:34:57 +0800

    Print free disk space at startup using FileSystem bean

--- src/main/java/com/example/spring_cliff/SpringCliffApplication.java
+++ src/main/java/com/example/spring_cliff/SpringCliffApplication.java
@@ -2,12 +2,17 @@ package com.example.spring_cliff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.unit.DataSize;
+import com.example.spring_cliff.core.FileSystem;
 
 @SpringBootApplication
 public class SpringCliffApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCliffApplication.class, args);
+		var ctx = SpringApplication.run(SpringCliffApplication.class, args);
+
+		var fs = ctx.getBean(FileSystem.class);
+		System.out.println("Free space: " + DataSize.ofBytes(fs.getFreeSpace()).toGigabytes() + "GB");
 	}
 
 }
