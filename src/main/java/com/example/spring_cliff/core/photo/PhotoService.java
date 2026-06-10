0b861446545d849423ddb32f6df18c91ebd5e028
Cliff <hzf510920@163.com>
2026-06-10 16:46:00 +0800

    Add PhotoService.

---
+++ src/main/java/com/example/spring_cliff/core/photo/PhotoService.java
@@ -0,0 +1,25 @@
+package com.example.spring_cliff.core.photo;
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring_cliff.core.FileSystem;
+
+@Service
+public class PhotoService {
+    final FileSystem fs;
+    
+    public PhotoService(FileSystem fs) {
+        this.fs = fs;
+    }
+
+    public Optional<byte[]> download(String name) {
+        try {
+            byte[] data = fs.load(name + ".jpg");
+            return Optional.of(data);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+}
